56ad75c71952a354f4fac5c3e0e2b46194cde1fc
jacques-onek <onekongajaque@gmail.com>
2025-07-18 00:45:49 +0100

    ajout de autre env google provider dans env config

--- lib/config.ts
+++ lib/config.ts
@@ -1,10 +1,9 @@
 
 
-
-
-
-
-
 export const config = {
   databaseurl:process.env.DATABASEURL,
+  google:{
+    clientid:process.env.AUTH_GOOGLE_ID,
+    secret:process.env.AUTH_GOOGLE_SECRET
+  }
 }
