73dfdbdd4b7e69befad41cf252e31f48130c8fb9
jacques-onek <onekongajaque@gmail.com>
2025-07-19 16:26:49 +0100

    quelque modif sur le notnull

--- database/drizzle.ts
+++ database/drizzle.ts
@@ -2,5 +2,7 @@ import { config } from '@/lib/config';
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 
+
+
 const sql = neon(config.databaseurl!);
-export const db = drizzle({ client: sql });
+export const db = drizzle({client:sql});
