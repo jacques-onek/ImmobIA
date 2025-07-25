571927927ea1d87aff07f99307b9737f68a0eaa1
jacques-onek <onekongajaque@gmail.com>
2025-07-25 19:21:33 +0100

    quelques modif

--- app/estate/page.tsx
+++ app/estate/page.tsx
@@ -1,9 +1,15 @@
+import Card from '@/components/Card'
+import { properties } from '@/constant/seedData'
 import React from 'react'
 
 const page = () => {
   return (
-    <main>
-      
+    <main className='px-4'>
+      <section className='grid grid-cols-1 lg:grid-cols-4 gap-5 '>
+         {properties.map((props,i) => (
+           <Card key={i} {...props}/>
+         ))}
+      </section>
     </main>
   )
 }
