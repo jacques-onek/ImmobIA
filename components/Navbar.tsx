528a46dfb87b97caa390eac6b8fb58cd5316772f
jacques-onek <onekongajaque@gmail.com>
2025-07-28 17:15:40 +0100

    fixe le design mobile

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,8 +9,8 @@ const Navbar = () => {
 
   const pathname = usePathname()
   return (
-    <header className=' fixed z-50 flex items-center justify-between rounded-b-md py-10 w-full px-10  h-5 shadow-card  bg-white'>
-      <section className='flex py-2 gap-8 rounded-full px-8 text-center h-fit bg-slate-50   border-transparent inset-1 border-0'>
+    <header className=' fixed z-50 flex shadow items-center justify-between rounded-b-xs py-10 w-full px-10  h-5   bg-white'>
+      <section className='flex py-2 gap-8 rounded-full px-8 text-center max-md:hidden h-fit bg-slate-50   border-transparent inset-1 border-0'>
         {navLinks.map(({title,href}) => (
            <Link key={href} href={href} className={cn("text-sm font-mono text-gray-700",pathname === href && "bg-white rounded-full shadow-sm text-center py-[3px] px-4 ")}>
             {title}
