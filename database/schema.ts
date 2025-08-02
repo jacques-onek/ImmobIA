9ac0daa02f365f6711c659c17f3954eebc976195
jacques-onek <onekongajaque@gmail.com>
2025-08-02 07:42:49 +0100

    je cree le schema du estate

--- database/schema.ts
+++ database/schema.ts
@@ -1,14 +1,11 @@
-import { pgEnum, pgTable, text, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
-
-
-
-
+import { integer, pgEnum, pgTable, text, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
 
 
 
 
 
 export const ROLE_ENUM = pgEnum("role",["ADMIN","USER","SELLER"])
+export const PROPERTY_TYPE_ENUM = pgEnum("type",["Appartement", "Maison", "Studio" ,"Immeuble" , "Duplex", "Penthouse"])
 
 export const users = pgTable("users", {
   id: uuid("id").notNull().primaryKey().defaultRandom().unique(),
@@ -20,3 +17,18 @@ export const users = pgTable("users", {
   providerId:text("provider_id"),
   created:timestamp("created_at").defaultNow()
 });
+
+export const estates = pgTable('estates',{
+    id:uuid("id").defaultRandom().primaryKey().notNull().unique(),  
+    PropertyName: varchar("Property_name",{length:500}),
+    Price:integer("Property_price").notNull(), 
+    Bedrooms:integer("Property_bedrooms"),
+    Bathrooms:integer("Property_bathrooms"),
+    Address:text("Property_address").notNull(),
+    Rating:integer("Property_rating"), 
+    Type: PROPERTY_TYPE_ENUM("type"),
+    Features:text("Property_feature"), 
+    Area:integer("Property_area"), 
+    Image:text("Property_image") 
+
+})
