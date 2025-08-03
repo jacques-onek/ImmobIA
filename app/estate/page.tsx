98b312550f37d1f54545254153031c539f83507d
jacques-onek <onekongajaque@gmail.com>
2025-08-03 19:51:51 +0100

    amelioration design

--- app/estate/page.tsx
+++ app/estate/page.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 const page = () => {
   return (
     <main className='px-4'>
-      <section className='grid grid-cols-1 lg:grid-cols-4 gap-5 '>
+      <section className='grid grid-cols-1 lg:grid-cols-4 gap-y-5 '>
          {properties.map((props,i) => (
            <Card key={i} {...props}/>
          ))}
