7224cfdfc7cfa2e1ce77e7261d627873f97f930a
jacques-onek <onekongajaque@gmail.com>
2025-08-12 22:33:29 +0100

    je cree les data du sidebar link pour l manager dashboard

--- constant/AppData.ts
+++ constant/AppData.ts
@@ -1,3 +1,10 @@
+import { ManagerSibarLink } from "@/types";
+import { GoHomeFill } from "react-icons/go";
+import { MdAddHomeWork } from "react-icons/md";
+import { MdBookmarkAdded } from "react-icons/md";
+import { TbWorldShare } from "react-icons/tb";
+import { LuChartPie } from "react-icons/lu"; 
+import { TbUserShare } from "react-icons/tb";
 
 export const navLinks = [
     {
@@ -20,4 +27,37 @@ export const navLinks = [
         title:"Add property",
         href:"/estate/Add-property"
     }
+]
+export const ManagerSideBarLink : ManagerSibarLink[] = [
+    {
+        icon:LuChartPie,
+        title:"Dashboard",
+        route:"/manager/dashboard"
+    },
+    {
+        icon:GoHomeFill,
+        title:"Mes Proprietes",
+        route:"/manager/Properties"
+    },
+    {
+        icon:MdAddHomeWork,
+        title:"Ajoutez propiete",
+        route:"/manager/create/property"
+    },
+    {
+        icon:TbUserShare,
+        title:"Clients",
+        route:"/manager/clients"
+    },
+    {
+        icon:MdBookmarkAdded,
+        title:"Enregistre",
+        route:"/manager/bookmark"
+    },
+    {
+        icon:TbWorldShare,
+        title:"Campagne",
+        route:"/manager/create/campagne"
+    },
+
 ]
