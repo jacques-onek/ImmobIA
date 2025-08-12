8f3112046d4392f50b8eb89af46c53287c00cd85
jacques-onek <onekongajaque@gmail.com>
2025-08-12 22:35:08 +0100

    je cree le types pour le data du dashboard manager

--- types/index.ts
+++ types/index.ts
@@ -1,12 +1,12 @@
 
-
-
-
-
-
-
 export interface SignUpType {
     FullName:string
     email:string
     password:string
+}
+
+export interface ManagerSibarLink {
+    icon:React.ElementType
+    title:string
+    route:string
 }
